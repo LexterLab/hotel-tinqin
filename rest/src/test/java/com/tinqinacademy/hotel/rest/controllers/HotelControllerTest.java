2f4976a62f06803211454fcd8528674104d440f2
moldovexc <alexanderparpulansky@gmail.com>
2024-07-16 11:01:23 +0300

    added integration tests for getting room id

--- rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/HotelControllerTest.java
+++ rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/HotelControllerTest.java
@@ -1,12 +1,16 @@
 package com.tinqinacademy.hotel.rest.controllers;
 
 import com.tinqinacademy.hotel.api.RestAPIRoutes;
+import com.tinqinacademy.hotel.api.models.constants.BathroomType;
+import com.tinqinacademy.hotel.api.models.constants.BedSize;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoomOutput;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -44,6 +48,29 @@ class HotelControllerTest {
                 .andExpect(jsonPath("$.roomIds[5]").value(roomIds.get(5)))
                 .andExpect(jsonPath("$.roomIds[6]").value(roomIds.get(6)))
                 .andExpect(jsonPath("$.roomIds[7]").value(roomIds.get(7)));
+    }
+
+    @Test
+    void shouldRespondWithOKAndRoomDataWhenGettingRoomById() throws Exception {
+        GetRoomOutput output = GetRoomOutput.builder()
+                .id("2")
+                .price(BigDecimal.valueOf(23232))
+                .floor(4)
+                .bedSize(BedSize.KING_SIZE)
+                .bathroomType(BathroomType.PRIVATE)
+                .datesOccupied(List.of(LocalDate.now()))
+                .build();
+
+        mockMvc.perform(get(RestAPIRoutes.GET_ROOM_DETAILS, output.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(output.getId()))
+                .andExpect(jsonPath("$.price").value(output.getPrice()))
+                .andExpect(jsonPath("$.floor").value(output.getFloor()))
+                .andExpect(jsonPath("$.bedSize").value(output.getBedSize().toString()))
+                .andExpect(jsonPath("$.bathroomType").value(output.getBathroomType().toString()))
+                .andExpect(jsonPath("$.datesOccupied").value(output.getDatesOccupied().getFirst().toString()));
+
+
     }
 
 }
