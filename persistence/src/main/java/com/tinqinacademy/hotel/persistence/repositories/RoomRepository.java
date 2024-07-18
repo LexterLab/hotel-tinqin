e01953ed7eecca875cabdce17fe72c5a41a7c615
moldovexc <alexanderparpulansky@gmail.com>
2024-07-18 21:46:14 +0300

    Added many to many save for room_Beds

--- persistence/src/main/java/com/tinqinacademy/hotel/persistence/repositories/RoomRepository.java
+++ persistence/src/main/java/com/tinqinacademy/hotel/persistence/repositories/RoomRepository.java
@@ -1,15 +1,21 @@
 package com.tinqinacademy.hotel.persistence.repositories;
 
+import com.tinqinacademy.hotel.persistence.mappers.RoomRowMapper;
 import com.tinqinacademy.hotel.persistence.models.bed.Bed;
 import com.tinqinacademy.hotel.persistence.models.room.Room;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 @RequiredArgsConstructor
-public class RoomRepository {
+public class RoomRepository implements AliExpressJPARepository<Room>{
     private final JdbcTemplate jdbcTemplate;
+    private final RoomRowMapper roomRowMapper;
 
     public void save(Room room) {
         String sql = "INSERT INTO rooms (id, room_no, room_bathroom_type, floor, price) " +
@@ -18,9 +24,30 @@ public class RoomRepository {
                 room.getPrice());
     }
 
-    public void addBedToRoom(Bed bed, Room room) {
-        String sql = "INSERT INTO room_beds (bed_id, room_id) " +
-                "VALUES (?, ?)";
-        jdbcTemplate.update(sql, bed.getId(), room.getId());
+    @Override
+    public List<Room> findAll() {
+        return List.of();
     }
+
+    @Override
+    public Optional<Room> findById(UUID id) {
+        String sql = "SELECT * FROM rooms WHERE id = ?";
+//        List<Room> rooms = jdbcTemplate.query(sql, roomRowMapper, id);
+//
+//        if (!rooms.isEmpty()) {
+//            return Optional.of(rooms.getFirst());
+//        }
+        return Optional.ofNullable(jdbcTemplate.queryForObject(sql, roomRowMapper, id));
+    }
+
+
+
+    public void saveRoomBeds(List<Bed> beds, Room room) {
+        for (Bed bed : beds) {
+            String sql = "INSERT INTO room_beds (bed_id, room_id) " +
+                    "VALUES (?, ?)";
+            jdbcTemplate.update(sql, bed.getId(), room.getId());
+        }
+    }
+
 }
