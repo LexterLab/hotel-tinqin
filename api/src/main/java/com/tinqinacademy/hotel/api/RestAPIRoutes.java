c31198c1d75fb693d7238bb92662dc5808059760
moldovexc <alexanderparpulansky@gmail.com>
2024-07-19 12:57:51 +0300

    added new path for sign up

--- api/src/main/java/com/tinqinacademy/hotel/api/RestAPIRoutes.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/RestAPIRoutes.java
@@ -4,6 +4,7 @@ public class RestAPIRoutes {
     public static final String ROOT = "/api/v1";
     public static final String HOTEL = ROOT + "/hotel";
     public static final String SYSTEM = ROOT + "/system";
+    public static final String AUTH = ROOT + "/auth";
     public static final String SEARCH_ROOMS = HOTEL + "/rooms";
     public static final String GET_ROOM_DETAILS = HOTEL + "/{roomId}";
     public static final String BOOK_ROOM = HOTEL + "/{roomId}";
@@ -14,4 +15,5 @@ public class RestAPIRoutes {
     public static final String UPDATE_ROOM = SYSTEM + "/room/{roomId}";
     public static final String PARTIAL_UPDATE_ROOM = SYSTEM + "/room/{roomId}";
     public static final String DELETE_ROOM = SYSTEM + "/room/{roomId}";
+    public static final String SIGN_UP = AUTH + "/signup";
 }
