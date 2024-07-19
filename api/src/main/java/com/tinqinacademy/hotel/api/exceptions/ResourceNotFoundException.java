b363464324e623356b2a97745e4544ecb5dbc684
moldovexc <alexanderparpulansky@gmail.com>
2024-07-19 12:58:07 +0300

    Added ResourceNotFoundException

--- api/src/main/java/com/tinqinacademy/hotel/api/exceptions/ResourceNotFoundException.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/exceptions/ResourceNotFoundException.java
@@ -14,12 +14,7 @@ public class ResourceNotFoundException extends RuntimeException {
     private Long fieldValue;
     private String fieldValueString;
 
-    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
-    }
+
 
     public ResourceNotFoundException(String resourceName, String fieldName, String fieldValueString) {
         super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValueString));
