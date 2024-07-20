59ecf366d8e73b80b3232a1958e43ca21ed70b3e
moldovexc <alexanderparpulansky@gmail.com>
2024-07-20 15:21:44 +0300

    Refactored book room to work with database

--- core/src/main/java/com/tinqinacademy/hotel/core/services/HotelServiceImpl.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/services/HotelServiceImpl.java
@@ -1,6 +1,8 @@
 package com.tinqinacademy.hotel.core.services;
 
 
+import com.tinqinacademy.hotel.api.exceptions.BookingDateNotAvailableException;
+import com.tinqinacademy.hotel.api.exceptions.ResourceNotFoundException;
 import com.tinqinacademy.hotel.api.models.constants.BathroomType;
 import com.tinqinacademy.hotel.api.models.constants.BedSize;
 import com.tinqinacademy.hotel.api.operations.bookroom.BookRoomInput;
@@ -12,22 +14,38 @@ import com.tinqinacademy.hotel.api.operations.searchroom.SearchRoomOutput;
 import com.tinqinacademy.hotel.api.operations.unbookroom.UnbookRoomInput;
 import com.tinqinacademy.hotel.api.operations.unbookroom.UnbookRoomOutput;
 import com.tinqinacademy.hotel.api.contracts.HotelService;
+
+import com.tinqinacademy.hotel.core.mappers.BookingMapper;
+import com.tinqinacademy.hotel.persistence.models.booking.Booking;
+import com.tinqinacademy.hotel.persistence.models.room.Room;
+
+import com.tinqinacademy.hotel.persistence.repositories.BookingRepository;
+import com.tinqinacademy.hotel.persistence.repositories.RoomRepository;
+import com.tinqinacademy.hotel.persistence.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class HotelServiceImpl implements HotelService {
-
+    private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
+    private final BookingRepository bookingRepository;
 
     @Override
     public SearchRoomOutput searchRoom(SearchRoomInput input) {
         log.info("Start searchRoom {}", input);
         List<String> roomIds = List.of("2", "3", "4", "5", "6", "7", "8", "9");
+
+
+
         SearchRoomOutput searchRoomOutput = SearchRoomOutput.builder()
                 .roomIds(roomIds).build();
         log.info("End searchRoom {}", searchRoomOutput);
@@ -52,6 +70,18 @@ public class HotelServiceImpl implements HotelService {
     @Override
     public BookRoomOutput bookRoom(BookRoomInput input) {
         log.info("Start bookRoom {}", input);
+
+        Room room = roomRepository.findById(input.getRoomId())
+                .orElseThrow(() -> new ResourceNotFoundException("Room", "Id", String.valueOf(input.getRoomId())));
+
+        if (bookingRepository.existByDates(input.getStartDate(), input.getEndDate(), room.getId())) {
+            throw new BookingDateNotAvailableException();
+        }
+
+        Booking booking = BookingMapper.INSTANCE.bookRoomInputToBooking(input);
+
+        bookingRepository.save(booking);
+
         BookRoomOutput output = BookRoomOutput.builder().build();
         log.info("End bookRoom {}", output);
         return output;
