50777c183b3f65a146f21d5aa8cc687585288108
moldovexc <alexanderparpulansky@gmail.com>
2024-07-20 15:21:19 +0300

    Added new constant class messages for keeping constants for exception messages

---
+++ api/src/main/java/com/tinqinacademy/hotel/api/Messages.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.hotel.api;
+
+public class Messages {
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
+    public static final String DATE_NOT_AVAILABLE = "Date not available";
+}
