8242d427007f74223f16fe27ca25c13b19e3e7b4
moldovexc <alexanderparpulansky@gmail.com>
2024-07-20 19:53:19 +0300

    Added new error messages

--- api/src/main/java/com/tinqinacademy/hotel/api/Messages.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/Messages.java
@@ -3,4 +3,6 @@ package com.tinqinacademy.hotel.api;
 public class Messages {
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
     public static final String DATE_NOT_AVAILABLE = "Date not available";
+    public static final String VISIT_ALREADY_FINISHED = "Visit already finished";
+    public static final String VISIT_ALREADY_STARTED = "Visit already started";
 }
