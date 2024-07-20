930e34055167689cf536a475c601da0dd646d493
moldovexc <alexanderparpulansky@gmail.com>
2024-07-20 21:04:07 +0300

    Added room mapper for mapping input to entity

---
+++ core/src/main/java/com/tinqinacademy/hotel/core/mappers/RoomMapper.java
@@ -0,0 +1,16 @@
+package com.tinqinacademy.hotel.core.mappers;
+
+import com.tinqinacademy.hotel.api.operations.createroom.CreateRoomInput;
+import com.tinqinacademy.hotel.persistence.models.room.Room;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface RoomMapper {
+    RoomMapper INSTANCE = Mappers.getMapper(RoomMapper.class);
+
+    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())")
+    Room createRoomInputToRoom(CreateRoomInput room);
+}
