c12980022ac1768344bbe70875a9f6ffc8fbe377
moldovexc <alexanderparpulansky@gmail.com>
2024-07-22 12:32:09 +0300

    Refactored error message to work with Messages constants

--- api/src/main/java/com/tinqinacademy/hotel/api/Messages.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/Messages.java
@@ -6,4 +6,6 @@ public class Messages {
     public static final String VISIT_ALREADY_FINISHED = "Visit already finished";
     public static final String VISIT_ALREADY_STARTED = "Visit already started";
     public static final String ROOM_NO_ALREADY_EXISTS = "Room number %s already exists";
+    public static final String GUEST_ALREADY_REGISTERED = "Guest with id %s already registered with booking: %s";
+    public static final String RESOURCE_NOT_FOUND = "%s not found with %s : '%s'";
 }
