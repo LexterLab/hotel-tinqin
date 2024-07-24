cf53d4720bf1c0e27a523d5d02756158142fa221
moldovexc <alexanderparpulansky@gmail.com>
2024-07-24 19:34:20 +0300

    Add entity and repo scanners for jpa

--- rest/src/main/java/com/tinqinacademy/hotel/rest/RestApplication.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/RestApplication.java
@@ -2,8 +2,12 @@ package com.tinqinacademy.hotel.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.tinqinacademy.hotel"})
+@EntityScan(basePackages = {"com.tinqinacademy.hotel.persistence.models"})
+@EnableJpaRepositories(basePackages = {"com.tinqinacademy.hotel.persistence.repositories"})
 public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
