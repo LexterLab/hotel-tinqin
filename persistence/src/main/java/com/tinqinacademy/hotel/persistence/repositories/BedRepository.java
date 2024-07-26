86e728e65a206b3900a5751ef087016a908b8ea2
moldovexc <alexanderparpulansky@gmail.com>
2024-07-26 20:45:41 +0300

    Add repo method for findingall beds by bed size

--- persistence/src/main/java/com/tinqinacademy/hotel/persistence/repositories/BedRepository.java
+++ persistence/src/main/java/com/tinqinacademy/hotel/persistence/repositories/BedRepository.java
@@ -4,9 +4,11 @@ import com.tinqinacademy.hotel.api.models.constants.BedSize;
 import com.tinqinacademy.hotel.persistence.models.bed.Bed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BedRepository extends JpaRepository<Bed, UUID> {
     Optional<Bed> findByBedSize(BedSize bedSize);
+    List<Bed> findAllByBedSizeIn(List<BedSize> bedSize);
 }
