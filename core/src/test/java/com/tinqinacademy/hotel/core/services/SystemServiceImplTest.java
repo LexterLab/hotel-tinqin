7ab201dfcb414579982c934cb14e837640b06f5f
moldovexc <alexanderparpulansky@gmail.com>
2024-07-29 10:26:43 +0300

    Unit tests for get guest report business logic

--- core/src/test/java/com/tinqinacademy/hotel/core/services/SystemServiceImplTest.java
+++ core/src/test/java/com/tinqinacademy/hotel/core/services/SystemServiceImplTest.java
@@ -2,7 +2,10 @@ package com.tinqinacademy.hotel.core.services;
 
 import com.tinqinacademy.hotel.api.exceptions.GuestAlreadyRegisteredException;
 import com.tinqinacademy.hotel.api.exceptions.ResourceNotFoundException;
+import com.tinqinacademy.hotel.api.operations.getguestreport.GetGuestReportInput;
+import com.tinqinacademy.hotel.api.operations.getguestreport.GetGuestReportOutput;
 import com.tinqinacademy.hotel.api.operations.guest.GuestInput;
+import com.tinqinacademy.hotel.api.operations.guest.GuestOutput;
 import com.tinqinacademy.hotel.api.operations.partialupdateroom.PartialUpdateRoomInput;
 import com.tinqinacademy.hotel.api.operations.registervisitor.RegisterGuestInput;
 import com.tinqinacademy.hotel.persistence.models.booking.Booking;
@@ -17,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -152,7 +156,62 @@ class SystemServiceImplTest {
         when(conversionService.convert(guestInput, Guest.class)).thenReturn(guest);
         when(guestRepository.findByIdCardNo(guestInput.getIdCardNo())).thenReturn(Optional.of(guest));
 
-       assertThrows(GuestAlreadyRegisteredException.class, () -> systemService.registerGuest(input));
+        assertThrows(GuestAlreadyRegisteredException.class, () -> systemService.registerGuest(input));
+    }
+
+
+    @Test
+    void shouldReturnGuestReports() {
+        GetGuestReportInput input = GetGuestReportInput
+                .builder()
+                .idCardNo("232 3 3232 32")
+                .idCardValidity(LocalDate.now().plusYears(2))
+                .idCardIssueAuthority("BULGARIA")
+                .idCardIssueDate(LocalDate.now().minusYears(2))
+                .firstName("Alex")
+                .lastName("Peter")
+                .startDate(LocalDateTime.now().plusYears(1))
+                .endDate(LocalDateTime.now().plusYears(1))
+                .phoneNo("+3598535353453")
+                .build();
+
+
+        Guest guest  = Guest
+                .builder()
+                .firstName(input.getFirstName())
+                .lastName(input.getLastName())
+                .idCardNo(input.getIdCardNo())
+                .idCardValidity(input.getIdCardValidity())
+                .idCardIssueAuthority(input.getIdCardIssueAuthority())
+                .idCardIssueDate(input.getIdCardIssueDate())
+                .build();
+
+        List<Guest> guests = new ArrayList<>(List.of(guest));
+
+        GuestOutput guestOutput = GuestOutput
+                .builder()
+                .firstName(input.getFirstName())
+                .lastName(input.getLastName())
+                .idCardNo(input.getIdCardNo())
+                .idCardValidity(input.getIdCardValidity())
+                .idCardIssueAuthority(input.getIdCardIssueAuthority())
+                .idCardIssueDate(input.getIdCardIssueDate())
+                .build();
+
+        GetGuestReportOutput expectedOutput = GetGuestReportOutput
+                .builder()
+                .guestsReports(List.of(guestOutput))
+                .build();
+
+        when(guestRepository.searchGuest(input.getStartDate(), input.getEndDate(),
+                input.getFirstName(), input.getLastName(), input.getPhoneNo(), input.getIdCardNo(),
+                input.getIdCardValidity(), input.getIdCardIssueAuthority(), input.getIdCardIssueDate(),
+                input.getRoomNo())).thenReturn(guests);
+        when(conversionService.convert(guest, GuestOutput.class)).thenReturn(guestOutput);
+
+        GetGuestReportOutput output = systemService.getGuestReport(input);
+
+        assertEquals(expectedOutput.toString(), output.toString());
     }
 
 }
