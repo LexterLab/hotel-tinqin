e0d43f0b405d68c5dae05ec390df12e45ea3eb75
moldovexc <alexanderparpulansky@gmail.com>
2024-07-29 11:00:20 +0300

    Unit tests for create room business logic

--- core/src/test/java/com/tinqinacademy/hotel/core/services/SystemServiceImplTest.java
+++ core/src/test/java/com/tinqinacademy/hotel/core/services/SystemServiceImplTest.java
@@ -1,15 +1,22 @@
 package com.tinqinacademy.hotel.core.services;
 
+import com.tinqinacademy.hotel.api.enumerations.BathroomType;
+import com.tinqinacademy.hotel.api.enumerations.BedSize;
 import com.tinqinacademy.hotel.api.exceptions.GuestAlreadyRegisteredException;
 import com.tinqinacademy.hotel.api.exceptions.ResourceNotFoundException;
+import com.tinqinacademy.hotel.api.exceptions.RoomNoAlreadyExistsException;
+import com.tinqinacademy.hotel.api.operations.createroom.CreateRoomInput;
+import com.tinqinacademy.hotel.api.operations.createroom.CreateRoomOutput;
 import com.tinqinacademy.hotel.api.operations.getguestreport.GetGuestReportInput;
 import com.tinqinacademy.hotel.api.operations.getguestreport.GetGuestReportOutput;
 import com.tinqinacademy.hotel.api.operations.guest.GuestInput;
 import com.tinqinacademy.hotel.api.operations.guest.GuestOutput;
 import com.tinqinacademy.hotel.api.operations.partialupdateroom.PartialUpdateRoomInput;
 import com.tinqinacademy.hotel.api.operations.registervisitor.RegisterGuestInput;
+import com.tinqinacademy.hotel.persistence.models.bed.Bed;
 import com.tinqinacademy.hotel.persistence.models.booking.Booking;
 import com.tinqinacademy.hotel.persistence.models.guest.Guest;
+import com.tinqinacademy.hotel.persistence.models.room.Room;
 import com.tinqinacademy.hotel.persistence.repositories.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +26,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -28,8 +36,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SystemServiceImplTest {
@@ -213,5 +220,69 @@ class SystemServiceImplTest {
 
         assertEquals(expectedOutput.toString(), output.toString());
     }
+
+    @Test
+    void shouldCreateRoom() {
+        CreateRoomInput input = CreateRoomInput
+                .builder()
+                .roomNo("201A")
+                .floor(4)
+                .beds(List.of(BedSize.KING_SIZE))
+                .price(BigDecimal.valueOf(200))
+                .bathroomType(BathroomType.PRIVATE)
+                .build();
+
+        List<com.tinqinacademy.hotel.persistence.enumerations.BedSize> bedSizes = input.beds().stream()
+                .map(bedSize -> com.tinqinacademy.hotel.persistence.enumerations.BedSize
+                        .getByCode(bedSize.toString()))
+                .toList();
+
+        Bed bed = Bed
+                .builder()
+                .bedCapacity(2)
+                .bedSize(com.tinqinacademy.hotel.persistence.enumerations.BedSize.KING_SIZE)
+                .build();
+
+        List<Bed> roomBeds = List.of(bed);
+
+        Room room = Room.builder()
+                .bathroomType(com.tinqinacademy.hotel.persistence.enumerations.BathroomType.PRIVATE)
+                .beds(roomBeds)
+                .price(input.price())
+                .floor(input.floor())
+                .roomNo(input.roomNo())
+                .id(UUID.randomUUID())
+                .build();
+
+        when(roomRepository.countAllByRoomNo(input.roomNo())).thenReturn(0L);
+        when(bedRepository.findAllByBedSizeIn(bedSizes)).thenReturn(roomBeds);
+        when(conversionService.convert(input, Room.class)).thenReturn(room);
+        when(roomRepository.save(room)).thenReturn(room);
+
+        CreateRoomOutput output = systemService.createRoom(input);
+
+        assertEquals(room.getId().toString(), output.getRoomId());
+    }
+
+    @Test
+    void shouldThrowRoomNoAlreadyExistsExceptionWhenCreatingRoomWithExistingRoomNo() {
+        CreateRoomInput input = CreateRoomInput
+                .builder()
+                .roomNo("201A")
+                .floor(4)
+                .beds(List.of(BedSize.KING_SIZE))
+                .price(BigDecimal.valueOf(200))
+
+                .bathroomType(BathroomType.PRIVATE)
+                .build();
+
+
+
+        when(roomRepository.countAllByRoomNo(input.roomNo())).thenReturn(1L);
+
+
+       assertThrows(RoomNoAlreadyExistsException.class, () -> systemService.createRoom(input));
+    }
+
 
 }
