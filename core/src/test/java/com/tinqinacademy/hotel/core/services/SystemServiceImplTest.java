999fd91fb6101955433de888a5a1a57eae2e8e7b
moldovexc <alexanderparpulansky@gmail.com>
2024-07-29 13:11:49 +0300

    Unit tests for update room business logic

--- core/src/test/java/com/tinqinacademy/hotel/core/services/SystemServiceImplTest.java
+++ core/src/test/java/com/tinqinacademy/hotel/core/services/SystemServiceImplTest.java
@@ -11,14 +11,14 @@ import com.tinqinacademy.hotel.api.operations.getguestreport.GetGuestReportInput
 import com.tinqinacademy.hotel.api.operations.getguestreport.GetGuestReportOutput;
 import com.tinqinacademy.hotel.api.operations.guest.GuestInput;
 import com.tinqinacademy.hotel.api.operations.guest.GuestOutput;
-import com.tinqinacademy.hotel.api.operations.partialupdateroom.PartialUpdateRoomInput;
 import com.tinqinacademy.hotel.api.operations.registervisitor.RegisterGuestInput;
+import com.tinqinacademy.hotel.api.operations.updateroom.UpdateRoomInput;
+import com.tinqinacademy.hotel.api.operations.updateroom.UpdateRoomOutput;
 import com.tinqinacademy.hotel.persistence.models.bed.Bed;
 import com.tinqinacademy.hotel.persistence.models.booking.Booking;
 import com.tinqinacademy.hotel.persistence.models.guest.Guest;
 import com.tinqinacademy.hotel.persistence.models.room.Room;
 import com.tinqinacademy.hotel.persistence.repositories.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -284,5 +284,99 @@ class SystemServiceImplTest {
        assertThrows(RoomNoAlreadyExistsException.class, () -> systemService.createRoom(input));
     }
 
+    @Test
+    void shouldUpdateRoom() {
+        UpdateRoomInput input = UpdateRoomInput
+                .builder()
+                .roomNo("202A")
+                .roomId(UUID.randomUUID())
+                .floor(4)
+                .beds(List.of(BedSize.KING_SIZE))
+                .bathroomType(BathroomType.PRIVATE)
+                .build();
+
+        Bed bed = Bed
+                .builder()
+                .bedSize(com.tinqinacademy.hotel.persistence.enumerations.BedSize.KING_SIZE)
+                .bedCapacity(20)
+                .build();
+
+        List<Bed> roomBeds = List.of(bed);
+
+        List<com.tinqinacademy.hotel.persistence.enumerations.BedSize> bedSizes = input.getBeds().stream()
+                .map(bedSize -> com.tinqinacademy.hotel.persistence.enumerations.BedSize.getByCode(bedSize.toString()))
+                .toList();
+
+        Room room = Room
+                .builder()
+                .bathroomType(com.tinqinacademy.hotel.persistence.enumerations.BathroomType.SHARED)
+                .roomNo("201A")
+                .floor(4)
+                .beds(List.of())
+                .price(BigDecimal.valueOf(200))
+                .build();
+
+        Room updatedRoom = Room
+                .builder()
+                .id(input.getRoomId())
+                .roomNo(input.getRoomNo())
+                .floor(input.getFloor())
+                .beds(roomBeds)
+                .price(input.getPrice())
+                .bathroomType(com.tinqinacademy.hotel.persistence.enumerations.BathroomType.getByCode(input.getBathroomType().toString()))
+                .build();
+
+
+        when(roomRepository.findById(input.getRoomId())).thenReturn(Optional.of(room));
+        when(bedRepository.findAllByBedSizeIn(bedSizes)).thenReturn(roomBeds);
+        when(conversionService.convert(input, Room.class)).thenReturn(updatedRoom);
+        when(roomRepository.save(updatedRoom)).thenReturn(updatedRoom);
+
+        UpdateRoomOutput output = systemService.updateRoom(input);
+
+        assertEquals(updatedRoom.getId(), output.getRoomId());
+    }
+
+    @Test
+    void shouldThrowResourceNotFoundWhenUpdatingUnexistingRoom() {
+        UpdateRoomInput input = UpdateRoomInput
+                .builder()
+                .roomNo("202A")
+                .roomId(UUID.randomUUID())
+                .floor(4)
+                .beds(List.of(BedSize.KING_SIZE))
+                .bathroomType(BathroomType.PRIVATE)
+                .build();
+
+        when(roomRepository.findById(input.getRoomId())).thenThrow(ResourceNotFoundException.class);
+
+        assertThrows(ResourceNotFoundException.class, () -> systemService.updateRoom(input));
+    }
+
+    @Test
+    void shouldThrowRoomNoAlreadyExistsExceptionWhenUpdatingRoomWithExistingRoomNo() {
+        UpdateRoomInput input = UpdateRoomInput
+                .builder()
+                .roomNo("202A")
+                .roomId(UUID.randomUUID())
+                .floor(4)
+                .beds(List.of(BedSize.KING_SIZE))
+                .bathroomType(BathroomType.PRIVATE)
+                .build();
+
+        Room room = Room
+                .builder()
+                .bathroomType(com.tinqinacademy.hotel.persistence.enumerations.BathroomType.SHARED)
+                .roomNo("201A")
+                .floor(4)
+                .beds(List.of())
+                .price(BigDecimal.valueOf(200))
+                .build();
+
+        when(roomRepository.findById(input.getRoomId())).thenReturn(Optional.of(room));
+        when(roomRepository.countAllByRoomNo(input.getRoomNo())).thenReturn(1L);
+
+        assertThrows(RoomNoAlreadyExistsException.class, () -> systemService.updateRoom(input));
+    }
 
 }
