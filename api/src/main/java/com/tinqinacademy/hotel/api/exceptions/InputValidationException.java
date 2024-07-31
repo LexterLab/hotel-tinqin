2f5543188b186d385854017b4c8256f839e075ff
moldovexc <alexanderparpulansky@gmail.com>
2024-07-31 19:17:19 +0300

    Refactor validator exception to work with list of errors instead of binding result

--- api/src/main/java/com/tinqinacademy/hotel/api/exceptions/InputValidationException.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/exceptions/InputValidationException.java
@@ -1,13 +1,20 @@
 package com.tinqinacademy.hotel.api.exceptions;
 
+import com.tinqinacademy.hotel.api.operations.errors.Error;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.core.MethodParameter;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class InputValidationException  extends RuntimeException{
-   private BindingResult bindingResult;
+public class InputValidationException  extends RuntimeException {
+   private List<Error> errors;
+
+
 }
