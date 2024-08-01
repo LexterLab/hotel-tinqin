e2889d78b59571e36b5635463858fd34047c0f99
moldovexc <alexanderparpulansky@gmail.com>
2024-08-01 19:26:25 +0300

    Add scan package for feign

--- rest/src/main/java/com/tinqinacademy/hotel/rest/RestApplication.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/RestApplication.java
@@ -3,11 +3,13 @@ package com.tinqinacademy.hotel.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.tinqinacademy.hotel"})
 @EntityScan(basePackages = {"com.tinqinacademy.hotel.persistence.models"})
 @EnableJpaRepositories(basePackages = {"com.tinqinacademy.hotel.persistence.repositories"})
+@EnableFeignClients(basePackages = {"com.tinqinacademy.hotel"})
 public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
