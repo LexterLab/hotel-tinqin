1b988ef065ce3d2618968c885c03ef875979b480
moldovexc <alexanderparpulansky@gmail.com>
2024-08-10 16:18:00 +0300

    Remove redundant error handlers

--- api/src/main/java/com/tinqinacademy/hotel/api/contracts/ErrorHandler.java
+++
@@ -1,7 +0,0 @@
-package com.tinqinacademy.hotel.api.contracts;
-
-import com.tinqinacademy.hotel.api.errors.ErrorOutput;
-
-public interface ErrorHandler {
-    ErrorOutput handle(Exception exception);
-}
