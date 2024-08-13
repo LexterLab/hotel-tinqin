24869a3a1c1208b431976aa62789a87408ab7485
moldovexc <alexanderparpulansky@gmail.com>
2024-08-13 18:35:19 +0300

    Remove user related params from search bookings repo method

--- persistence/src/main/java/com/tinqinacademy/hotel/persistence/repositories/BookingRepository.java
+++ persistence/src/main/java/com/tinqinacademy/hotel/persistence/repositories/BookingRepository.java
@@ -16,13 +16,11 @@ public interface BookingRepository extends JpaRepository<Booking, UUID> {
     @Query("""
         SELECT b FROM Booking b
         JOIN b.guests g
-        JOIN b.user u
         JOIN b.room r
         WHERE (CAST(:startDate AS DATE) IS NULL OR b.startDate >= :startDate)
         AND (CAST(:endDate AS DATE) IS NULL OR b.endDate <= :endDate)
         AND (:firstName IS NULL OR g.firstName LIKE :firstName)
         AND (:lastName IS NULL OR g.lastName LIKE :lastName)
-        AND (:phoneNo IS NULL OR u.phoneNo LIKE :phoneNo)
         AND (:idCardNo IS NULL OR g.idCardNo LIKE :idCardNo)
         AND (CAST(:idCardValidity AS DATE) IS NULL OR g.idCardValidity = :idCardValidity)
         AND (:idCardIssueAuthority IS NULL OR g.idCardIssueAuthority = :idCardIssueAuthority)
@@ -30,7 +28,7 @@ public interface BookingRepository extends JpaRepository<Booking, UUID> {
         AND (:roomNo IS NULL OR r.roomNo = :roomNo)
     """)
     List<Booking> searchBookings(LocalDateTime startDate, LocalDateTime endDate, String firstName, String lastName,
-                            String phoneNo, String idCardNo, LocalDate idCardValidity, String idCardIssueAuthority,
+                           String idCardNo, LocalDate idCardValidity, String idCardIssueAuthority,
                             LocalDate idCardIssueDate, String roomNo);
 
     @Query(value = """
