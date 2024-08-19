19cf814928127d6f810770b34a5e584e46c812de
moldovexc <alexanderparpulansky@gmail.com>
2024-08-19 22:01:52 +0300

    Refactor script to mvn clean package every microservice before composing

--- docker-script.sh
+++ docker-script.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-mvn clean package
+mvn clean package -DskipTests
+
+projects=(
+    "../email-tinqin/pom.xml"
+    "../admin-tinqin/pom.xml"
+    "../bff-tinqin/pom.xml"
+    "../authentication-api/pom.xml"
+    "../comments-api-tinqin/pom.xml"
+)
+
+for project in "${projects[@]}"
+do
+    echo "Building project: $project"
+    mvn clean package -f "$project" -DskipTests
+    if [ $? -ne 0 ]; then
+        echo "Build failed for $project. Exiting."
+        exit 1
+    fi
+done
 
 docker-compose down
 
