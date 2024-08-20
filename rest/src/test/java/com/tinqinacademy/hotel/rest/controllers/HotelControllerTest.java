295113295d72efa0c89485dcf76c27ff0fe215d8
moldovexc <alexanderparpulansky@gmail.com>
2024-08-20 20:01:45 +0300

    IT for search available rooms

--- rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/HotelControllerTest.java
+++ rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/HotelControllerTest.java
@@ -1,59 +1,40 @@
-//package com.tinqinacademy.hotel.rest.controllers;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.tinqinacademy.hotel.api.RestAPIRoutes;
-//import com.tinqinacademy.hotel.api.models.constants.BathroomType;
-//import com.tinqinacademy.hotel.api.models.constants.BedSize;
-//import com.tinqinacademy.hotel.api.operations.bookroom.BookRoomInput;
-//import com.tinqinacademy.hotel.api.operations.getroom.GetRoomOutput;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class HotelControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @Test
-//    void shouldRespondWithOkAndAvailableRoomsWhenSearchingAvailableRooms() throws Exception {
-//        List<String> roomIds = List.of("2", "3", "4", "5", "6", "7", "8", "9");
-//
-//        mockMvc.perform(get(RestAPIRoutes.SEARCH_ROOMS)
-//                .param("startDate", String.valueOf(LocalDate.now()))
-//                .param("endDate", String.valueOf(LocalDate.now()))
-//                        .param("bedCount", String.valueOf(10))
-//                        .param("bedSize", "single")
-//                        .param("bathroomType", "shared"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.roomIds").isNotEmpty())
-//                .andExpect(jsonPath("$.roomIds").isArray())
-//                .andExpect(jsonPath("$.roomIds[0]").value(roomIds.getFirst()))
-//                .andExpect(jsonPath("$.roomIds[1]").value(roomIds.get(1)))
-//                .andExpect(jsonPath("$.roomIds[2]").value(roomIds.get(2)))
-//                .andExpect(jsonPath("$.roomIds[3]").value(roomIds.get(3)))
-//                .andExpect(jsonPath("$.roomIds[4]").value(roomIds.get(4)))
-//                .andExpect(jsonPath("$.roomIds[5]").value(roomIds.get(5)))
-//                .andExpect(jsonPath("$.roomIds[6]").value(roomIds.get(6)))
-//                .andExpect(jsonPath("$.roomIds[7]").value(roomIds.get(7)));
-//    }
-//
+package com.tinqinacademy.hotel.rest.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tinqinacademy.hotel.api.RestAPIRoutes;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class HotelControllerTest extends BaseIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldRespondWithOkAndAvailableRoomsWhenSearchingAvailableRooms() throws Exception {
+
+        mockMvc.perform(get(RestAPIRoutes.SEARCH_ROOMS)
+                        .param("startDate", String.valueOf(LocalDateTime.now()))
+                        .param("endDate", String.valueOf(LocalDateTime.now())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.roomIds").isNotEmpty())
+                .andExpect(jsonPath("$.roomIds").isArray());
+    }
+
 //    @Test
 //    void shouldRespondWithOKAndRoomDataWhenGettingRoomById() throws Exception {
 //        GetRoomOutput output = GetRoomOutput.builder()
@@ -406,4 +387,4 @@
 //                .andExpect(jsonPath("$").isEmpty());
 //    }
 //
-//}
+}
