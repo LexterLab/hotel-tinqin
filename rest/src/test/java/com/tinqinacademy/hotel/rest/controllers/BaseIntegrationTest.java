11642031290e2b58dabd1d34cabe17d44e4c1287
moldovexc <alexanderparpulansky@gmail.com>
2024-08-20 19:39:09 +0300

    Add tearDown for flyway for pure tests

--- rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/BaseIntegrationTest.java
+++ rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/BaseIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.tinqinacademy.hotel.rest.controllers;
 
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,6 +14,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@SpringBootTest
 public class BaseIntegrationTest {
 
     @Container
@@ -18,6 +23,15 @@ public class BaseIntegrationTest {
             .withDatabaseName("test")
             .withUsername("test")
             .withPassword("test");
+
+    @Autowired
+    private Flyway flyway;
+
+    @AfterEach
+    void tearDown() {
+        flyway.clean();
+        flyway.migrate();
+    }
 
     @BeforeAll
     static void beforeAll() {
@@ -38,5 +52,7 @@ public class BaseIntegrationTest {
         registry.add("spring.flyway.url", postgres::getJdbcUrl);
         registry.add("spring.flyway.user", postgres::getUsername);
         registry.add("spring.flyway.password", postgres::getPassword);
+
+        registry.add("spring.flyway.clean-disabled", () -> false);
     }
 }
