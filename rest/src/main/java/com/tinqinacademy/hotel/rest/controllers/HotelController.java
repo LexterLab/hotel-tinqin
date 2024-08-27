5c339d5024b1fe95a0e0a24f648dd64f13c735d5
moldovexc <alexanderparpulansky@gmail.com>
2024-08-27 21:37:40 +0300

    Extract Rest export processor to separate project

--- rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/HotelController.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/HotelController.java
@@ -19,7 +19,7 @@ import com.tinqinacademy.hotel.api.operations.unbookroom.UnbookRoomInput;
 import com.tinqinacademy.hotel.api.operations.unbookroom.UnbookRoomOutput;
 import com.tinqinacademy.hotel.api.RestAPIRoutes;
 import com.tinqinacademy.hotel.api.operations.unbookroom.UnbookRoom;
-import com.tinqinacademy.hotel.restexportprocessor.RestExport;
+import com.tinqinacademy.restexportprocessor.main.RestExport;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
